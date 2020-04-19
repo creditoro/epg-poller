c5394ca63f03ca27edc732d3393e528a2015e5fc
kepet19 <kevinmeejach@gmail.com>
2020-04-19 23:09:39 +0200

    Working againg tommorow, it can post channels now, It just need major refactoring.

--- src/main/java/dk/creditoro/epgPoller/EpgPoller.java
+++ src/main/java/dk/creditoro/epgPoller/EpgPoller.java
@@ -6,24 +6,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONObject;
+
+import dk.creditoro.epgPoller.networking.rest_client.endpoints.HttpManager;
+import kong.unirest.*;
+import kong.unirest.json.JSONObject;
 
 
 // Get From each channel
@@ -33,17 +33,81 @@ import org.json.JSONObject;
 // Channels, id number,  title String , icon url(String), 
 // https://tvtid-api.api.tv2.dk/api/tvtid/v1/schedules/channels
 
-
 // TODO Make a class that holdes a channel, with the atribues ind parser. 
 // Make a Terminal frontend, to chose what to download.
 // Make a downloader for a channel, så it kan hold programs. 
 // Make a class that is a program. 
 // Maybe make a Hashmap of programs to add to Creditoro api.
 public class EpgPoller {
+    		private static final String AUTH_HEADER = "Authorization";
+    		private static final String IDENTIFIER = "identifier";
+    		private static final String PATH = "/%s/{%s}";
+			private List<ChannelData> channelList;
+			private HttpManager httpManager;
+    		private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+			private String token;
+
+			public EpgPoller (){
+				channelList = new ArrayList<>();
+				httpManager = new HttpManager();
+			}
+
+	public String parse (String responebody){
+		//JSONArray channels = new JSONArray(responebody);
+		org.json.JSONObject channels = new org.json.JSONObject(responebody);
+		JSONArray channelArray = channels.getJSONArray("channels");
+			for(int i=0; i < channelArray.length(); i++){
+			org.json.JSONObject channel = channelArray.getJSONObject(i);
+				int id = channel.getInt("id");
+				String title = channel.getString("title");
+				String icon = channel.getString("icon");
+				String logo = channel.getString("logo");
+				String svglogo = channel.getString("svgLogo");
+				// Not all have lang feels 
+				// TODO fix lang input
+				// String lang = channel.getString("lang");
+				int sort = channel.getInt("sort");
+				System.out.println(id + " " + title + " " + icon + " " + sort);
+				channelList.add(new ChannelData(title, id, icon, logo, svglogo));
+	  	LOGGER.info("Add one");
+	}
+			return "Hello world";
+	}
+
+
+	public String getToken(){
+		return token;
+	}
+
+    public void postLogin(String email, String password) {
+        var body = new JSONObject(Map.of("email", email, "password", password));
+        var response = httpManager.post("/users/login", body);
+        token = response.asJson().getBody().getObject().getString("token");
+    }
+
+    public RequestBodyEntity put(String route, JSONObject body, String token) {
+        return Unirest.post(route)
+                .body(body)
+                .header(AUTH_HEADER, token);
+    }
+
+	public void addtoServer(){
+		
+		//RequestBodyEntity requestBodyEntity = httpManager.post("user", body);
+		for (ChannelData channelData : channelList) {
+
+			System.out.println(channelData.getName());
+			var body = new JSONObject(Map.of("name", channelData.getName()));
+			System.out.println(body.toString());
+			RequestBodyEntity id = put("/channels/", body, token);
+			LOGGER.info("Add channel: " + channelData.getName() + " id: " + id.asJson().getStatus() + " To the server.");
+		}
+	}
+
 	public static void main(String[] args) {
+			EpgPoller epgPoller = new EpgPoller();
 			CloseableHttpResponse response1 = null;
 			CloseableHttpClient httpclient = HttpClients.createDefault();
-			StringBuffer responseContent = new StringBuffer();
 		try {
 			HttpHost target = new HttpHost("tvtid-api.api.tv2.dk", 80, "http");
 			HttpGet getRequest = new HttpGet("/api/tvtid/v1/schedules/channels"); // KIG KIG KIG HER :D 
@@ -57,7 +121,7 @@ public class EpgPoller {
 			
 			//Check what kind of data that comes ind
 			//System.out.println(content);
-			parse(content);
+			epgPoller.parse(content);
 			// do something useful with the response body
 			// and ensure it is fully consumed
 			EntityUtils.consume(entity1);
@@ -73,25 +137,10 @@ public class EpgPoller {
 				System.out.println(e);
 			}
 		}
-	}
-	public static String parse (String responebody){
-		//JSONArray channels = new JSONArray(responebody);
-		JSONObject channels = new JSONObject(responebody);
-		JSONArray channelArray = channels.getJSONArray("channels");
-		for(int i=0; i < channelArray.length(); i++){
-			JSONObject channel = channelArray.getJSONObject(i);
-			int id = channel.getInt("id");
-			String title = channel.getString("title");
-			String icon = channel.getString("icon");
-			String logo = channel.getString("logo");
-			String svglogo = channel.getString("svgLogo");
-			// Not all have lang feels 
-			// TODO fix lang input
-			// String lang = channel.getString("lang");
-			int sort = channel.getInt("sort");
-			System.out.println(id + " " + title + " " + icon + " " + sort);
-		}
-			return "Hello world";
+
+		epgPoller.postLogin("string@string.dk", "string");
+		LOGGER.info("Logged ind with token: " + epgPoller.getToken());
+		epgPoller.addtoServer();
 	}
 }
 
