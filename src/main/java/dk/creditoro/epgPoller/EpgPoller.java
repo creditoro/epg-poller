dffb74fa22c206843fee0c9091bdbcd863be4430
Kechr <kechr19@student.sdu.dk>
2020-04-20 22:57:39 +0200

    Added icon_url in addToServer()

--- src/main/java/dk/creditoro/epgPoller/EpgPoller.java
+++ src/main/java/dk/creditoro/epgPoller/EpgPoller.java
@@ -33,51 +33,51 @@ import kong.unirest.json.JSONObject;
 // Channels, id number,  title String , icon url(String), 
 // https://tvtid-api.api.tv2.dk/api/tvtid/v1/schedules/channels
 
-// TODO Make a class that holdes a channel, with the atribues ind parser. 
+// TODO Make a class that holds a channel, with the attributes ind parser.
 // Make a Terminal frontend, to chose what to download.
-// Make a downloader for a channel, så it kan hold programs. 
+// Make a downloader for a channel, so it kan hold programs.
 // Make a class that is a program. 
 // Maybe make a Hashmap of programs to add to Creditoro api.
 public class EpgPoller {
-    		private static final String AUTH_HEADER = "Authorization";
-    		private static final String IDENTIFIER = "identifier";
-    		private static final String PATH = "/%s/{%s}";
-			private List<ChannelData> channelList;
-			private HttpManager httpManager;
-    		private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-			private String token;
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String IDENTIFIER = "identifier";
+    private static final String PATH = "/%s/{%s}";
+    private List<ChannelData> channelList;
+    private HttpManager httpManager;
+    private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private String token;
 
-			public EpgPoller (){
-				channelList = new ArrayList<>();
-				httpManager = new HttpManager();
-			}
+    public EpgPoller() {
+        channelList = new ArrayList<>();
+        httpManager = new HttpManager();
+    }
 
-	public String parse (String responebody){
-		//JSONArray channels = new JSONArray(responebody);
-		org.json.JSONObject channels = new org.json.JSONObject(responebody);
-		JSONArray channelArray = channels.getJSONArray("channels");
-			for(int i=0; i < channelArray.length(); i++){
-			org.json.JSONObject channel = channelArray.getJSONObject(i);
-				int id = channel.getInt("id");
-				String title = channel.getString("title");
-				String icon = channel.getString("icon");
-				String logo = channel.getString("logo");
-				String svglogo = channel.getString("svgLogo");
-				// Not all have lang feels 
-				// TODO fix lang input
-				// String lang = channel.getString("lang");
-				int sort = channel.getInt("sort");
-				System.out.println(id + " " + title + " " + icon + " " + sort);
-				channelList.add(new ChannelData(title, id, icon, logo, svglogo));
-	  	LOGGER.info("Add one");
-	}
-			return "Hello world";
-	}
+    public String parse(String responseBody) {
+        //JSONArray channels = new JSONArray(responseBody);
+        org.json.JSONObject channels = new org.json.JSONObject(responseBody);
+        JSONArray channelArray = channels.getJSONArray("channels");
+        for (int i = 0; i < channelArray.length(); i++) {
+            org.json.JSONObject channel = channelArray.getJSONObject(i);
+            int id = channel.getInt("id");
+            String title = channel.getString("title");
+            String icon = channel.getString("icon");
+            String logo = channel.getString("logo");
+            String svgLogo = channel.getString("svgLogo");
+            // Not all have lang feels
+            // TODO fix lang input
+            // String lang = channel.getString("lang");
+            int sort = channel.getInt("sort");
+            System.out.println(id + " " + title + " " + icon + " " + sort);
+            channelList.add(new ChannelData(title, id, icon, logo, svgLogo));
+            LOGGER.info("Add one");
+        }
+        return "Hello world";
+    }
 
 
-	public String getToken(){
-		return token;
-	}
+    public String getToken() {
+        return token;
+    }
 
     public void postLogin(String email, String password) {
         var body = new JSONObject(Map.of("email", email, "password", password));
@@ -91,56 +91,60 @@ public class EpgPoller {
                 .header(AUTH_HEADER, token);
     }
 
-	public void addtoServer(){
-		
-		//RequestBodyEntity requestBodyEntity = httpManager.post("user", body);
-		for (ChannelData channelData : channelList) {
+    public void addtoServer() {
 
-			System.out.println(channelData.getName());
-			var body = new JSONObject(Map.of("name", channelData.getName()));
-			System.out.println(body.toString());
-			RequestBodyEntity id = put("/channels/", body, token);
-			LOGGER.info("Add channel: " + channelData.getName() + " id: " + id.asJson().getStatus() + " To the server.");
-		}
-	}
+        //RequestBodyEntity requestBodyEntity = httpManager.post("user", body);
+        for (ChannelData channelData : channelList) {
 
-	public static void main(String[] args) {
-			EpgPoller epgPoller = new EpgPoller();
-			CloseableHttpResponse response1 = null;
-			CloseableHttpClient httpclient = HttpClients.createDefault();
-		try {
-			HttpHost target = new HttpHost("tvtid-api.api.tv2.dk", 80, "http");
-			HttpGet getRequest = new HttpGet("/api/tvtid/v1/schedules/channels"); // KIG KIG KIG HER :D 
+            System.out.println(channelData.getName());
+            var body = new JSONObject(
+                    Map.of(
+                            "name", channelData.getName(),
+                            "icon_url", channelData.getIcon()
+                    )
+            );
+            System.out.println(body.toString());
+            RequestBodyEntity id = put("/channels/", body, token);
+            LOGGER.info("Add channel: " + channelData.getName() + " id: " + id.asJson().getStatus() + " To the server.");
+        }
+    }
 
-			response1 = httpclient.execute(target, getRequest);
-			System.out.println("hello world");	
-			System.out.println(response1.getStatusLine());
-			HttpEntity entity1 = response1.getEntity();
-			String content = new BufferedReader(new InputStreamReader( entity1.getContent() ))
-				.lines().collect(Collectors.joining("\n"));
-			
-			//Check what kind of data that comes ind
-			//System.out.println(content);
-			epgPoller.parse(content);
-			// do something useful with the response body
-			// and ensure it is fully consumed
-			EntityUtils.consume(entity1);
-		} catch(ClientProtocolException e){
-			System.out.println(e);
-		} catch(IOException e){
-			System.out.println(e);
-		}
-		finally {
-			try {
-			response1.close();
-			} catch(IOException e) {
-				System.out.println(e);
-			}
-		}
+    public static void main(String[] args) {
+        EpgPoller epgPoller = new EpgPoller();
+        CloseableHttpResponse response1 = null;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpHost target = new HttpHost("tvtid-api.api.tv2.dk", 80, "http");
+            HttpGet getRequest = new HttpGet("/api/tvtid/v1/schedules/channels"); // KIG KIG KIG HER :D
 
-		epgPoller.postLogin("string@string.dk", "string");
-		LOGGER.info("Logged ind with token: " + epgPoller.getToken());
-		epgPoller.addtoServer();
-	}
+            response1 = httpclient.execute(target, getRequest);
+            System.out.println("hello world");
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String content = new BufferedReader(new InputStreamReader(entity1.getContent()))
+                    .lines().collect(Collectors.joining("\n"));
+
+            //Check what kind of data that comes ind
+            //System.out.println(content);
+            epgPoller.parse(content);
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity1);
+        } catch (ClientProtocolException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                response1.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        epgPoller.postLogin("string@string.dk", "string");
+        LOGGER.info("Logged ind with token: " + epgPoller.getToken());
+        epgPoller.addtoServer();
+    }
 }
 
