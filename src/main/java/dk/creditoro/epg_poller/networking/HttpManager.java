734f1e8db5c8a389b2e888827031c409b6fe8056
kepet19 <kevinmeejach@gmail.com>
2020-05-12 14:08:28 +0200

    Maybe this time I fixed the code smels ind HttpManager

--- src/main/java/dk/creditoro/epg_poller/networking/HttpManager.java
+++ src/main/java/dk/creditoro/epg_poller/networking/HttpManager.java
@@ -92,7 +92,7 @@ public class HttpManager {
 					LOGGER.log(Level.INFO, HTTPERRORW, status);
 					Thread.sleep(5000);
 				} catch (InterruptedException e) {
-					LOGGER.log(Level.WARNING, "Thread got woken UP?: {1}", e.toString());
+					LOGGER.log(Level.INFO, "Thread got woken UP?: {0}", e.toString());
 					Thread.currentThread().interrupt();
 				}
 			} else {
@@ -129,7 +129,7 @@ public class HttpManager {
 					LOGGER.log(Level.INFO, HTTPERRORW, status);
 					Thread.sleep(5000);
 				} catch (InterruptedException e) {
-					LOGGER.log(Level.WARNING, "Thread got woken UP? {1}", e.toString());
+					LOGGER.log(Level.INFO, "Thread got woken UP?: {0}", e);
 					Thread.currentThread().interrupt();
 				}
 			} else {
