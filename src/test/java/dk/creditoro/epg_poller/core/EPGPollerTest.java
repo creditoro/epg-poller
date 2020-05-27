eecc63020d76ad240ac655946d1a081620bd7381
kepet19 <kevinmeejach@gmail.com>
2020-05-27 12:18:48 +0200

    Add the last test back, to get more codecovrage

--- src/test/java/dk/creditoro/epg_poller/core/EPGPollerTest.java
+++ src/test/java/dk/creditoro/epg_poller/core/EPGPollerTest.java
@@ -19,4 +19,13 @@ class EPGPollerTest {
 		assertNotNull(epgPoller, "Checks if it can make the epgPoller");
 	}
 
+	@Test 
+	void runningEPGPoller(){
+		assertDoesNotThrow(() -> epgPoller.start(), "EPGPoller can not run, check the assertMessage");
+	}
+
+	@Test 
+	void runningEPGPollerProduction(){
+		assertDoesNotThrow(() -> epgPoller.startPostProductions(), "EPGPoller can not run, check the assertMessage");
+	}
 }
